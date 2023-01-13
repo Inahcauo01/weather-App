9dfff689222f56317c5c3f049ddbb9593ce46ee1
tarek <tarek.ouachani01@gmail.com>
2023-01-13 08:16:07 +0000

    adding images of weather

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,6 +4,7 @@ const key = "ccb2c16eac97b4ec82818dee69aa8718";
 let city = document.querySelector("#city")
 let form = document.querySelector("#form");
 let recherche = document.querySelector("#recherche");
+let show = document.querySelector(".show");
 
 const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ccb2c16eac97b4ec82818dee69aa8718`;
 
@@ -19,6 +20,12 @@ async function getWeather(city){
     afficherData(repData);
 }
 
-afficherData(repData){
-    
+function afficherData(data){
+    console.log(data);
+    show.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+                    <br>${Math.floor((data.main.temp - (273.15)))}°C
+                    <br>${data.weather[0].main}
+                    <br>${data.weather[0].description}
+                    `;
+
 }
